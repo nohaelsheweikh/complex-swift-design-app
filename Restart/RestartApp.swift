0e93586ddbfc84430cc3728dfd9a2d39628ea1d7
nohahamdy <nohaelsheweikh@gmail.com>
2023-10-04 16:50:55 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Noha Hamdy on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
