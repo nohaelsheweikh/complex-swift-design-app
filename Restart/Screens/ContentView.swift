6f7e1dfecb270b98ff3d0fdc8d101f211cdb9d2e
nohahamdy <nohaelsheweikh@gmail.com>
2023-10-07 17:33:56 +0300

    onboarding screen

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Noha Hamdy on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive{
+                OnboardingView()
+            }else{
+                HomeView()
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
